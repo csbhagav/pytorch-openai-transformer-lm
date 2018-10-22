85f88d571b9a837c89a172f1cce81e86c49bab65
chandrab <chandrab@allenai.org>
2018-10-22 13:33:44 -0700

    Fix dataset reader call

--- analysis.py
+++ analysis.py
@@ -9,7 +9,7 @@ from datasets import _rocstories
 
 def rocstories(data_dir, pred_path, log_path):
     preds = pd.read_csv(pred_path, delimiter='\t')['prediction'].values.tolist()
-    _, _, _, labels = _rocstories(os.path.join(data_dir, 'test.csv'))
+    _, _, _, _, labels = _rocstories(os.path.join(data_dir, 'test.csv'))
     test_accuracy = accuracy_score(labels, preds)*100.
     logs = [json.loads(line) for line in open(log_path)][1:]
     best_validation_index = np.argmax([log['va_acc'] for log in logs])
